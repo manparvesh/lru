b2e1f29c9d4939d3b8943ab50522ef1c265ea9ca
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-08-11 15:17:20 +0800

    update g++ version

--- cpp/test.sh
+++ cpp/test.sh
@@ -2,7 +2,7 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-g++ lru.cpp
+g++ -std=c++11 lru.cpp
 if [ $? -eq 0 ]; then
     echo "${green}Compilation OK${reset}"
     ./a.out < in > out
